6904a2f553ebca8ced3f728e2f0bdf7852b262e5
Velmurugan <velmurugan.murugesan@gmail.com>
2021-08-10 21:46:25 +0530

    removed base project

--- app/src/main/java/com/velmurugan/jetpackcomposeexample/Model.kt
+++
@@ -1,3 +0,0 @@
-package com.velmurugan.jetpackcomposeexample
-
-data class Movie(val name: String, val category: String, val imageUrl: String, val desc: String)
--- app/src/main/java/com/velmurugan/jetpackcomposeexample/ui/LayoutScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.velmurugan.jetpackcomposeexample.ui
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun LayoutScreen() {
-
-
-}
