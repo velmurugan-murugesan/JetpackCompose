dfc5052d18093e84ffde06268309687fb66f634c
velmurugan.murugesan <velmurugan.murugesan@gmail.com>
2022-11-11 01:07:23 +0530

    added runtime permission sample

---
+++ RuntimePermissionJetpackCompose/app/src/main/java/com/example/runtimepermissionjetpackcompose/MainActivity.kt
@@ -0,0 +1,224 @@
+package com.example.runtimepermissionjetpackcompose
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.example.runtimepermissionjetpackcompose.ui.theme.RuntimePermissionJetpackComposeTheme
+import com.google.accompanist.permissions.*
+import java.security.Permission
+import java.util.*
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RuntimePermissionJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+                ) {
+                    //ContentActivityResultPermission()
+                    ContentAccompanistLibPermission()
+                }
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun ContentAccompanistLibPermission() {
+
+    val context = LocalContext.current
+    val file = context.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        BuildConfig.APPLICATION_ID + ".provider", file
+    )
+    var isShowCamera by remember {
+        mutableStateOf(false)
+    }
+    var capturedImageUri by remember {
+        mutableStateOf<Uri>(Uri.EMPTY)
+    }
+    val cameraLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+            isShowCamera = false
+            capturedImageUri = uri
+        }
+
+    //val permissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+
+    val permissionsState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    )
+
+    PermissionsRequired(
+        multiplePermissionsState = permissionsState,
+        permissionsNotGrantedContent = {
+            Toast.makeText(context, "Permission not granted", Toast.LENGTH_SHORT).show()
+        },
+        permissionsNotAvailableContent = {
+            Toast.makeText(context, "Permission not available", Toast.LENGTH_SHORT).show()
+        }) {
+        //content
+        /*if (isShowCamera) {
+            cameraLauncher.launch(uri)
+            isShowCamera = false
+        }*/
+    }
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(12.dp)) {
+        Button(onClick = {
+            permissionsState.permissions.all {
+                ContextCompat.checkSelfPermission(
+                    context,
+                    it.permission
+                ) == PackageManager.PERMISSION_GRANTED
+            }
+            if(permissionsState.permissions.all {
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        it.permission
+                    ) == PackageManager.PERMISSION_GRANTED
+                }) {
+                // access location
+               // cameraLauncher.launch(uri)
+            } else {
+                isShowCamera = true
+                permissionsState.launchMultiplePermissionRequest()
+            }
+        }) {
+            Text(text = "Location Permission")
+        }
+
+        if (capturedImageUri.path?.isNotEmpty() == true) {
+            Image(
+                modifier = Modifier
+                    .padding(16.dp, 8.dp)
+                    .fillMaxWidth()
+                    .size(400.dp),
+                painter = rememberImagePainter(capturedImageUri),
+                contentDescription = null
+            )
+        }
+
+    }
+
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun ContentActivityResultPermission(
+
+) {
+
+    val context = LocalContext.current
+    val file = context.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        BuildConfig.APPLICATION_ID + ".provider", file
+    )
+
+    var capturedImageUri by remember {
+        mutableStateOf<Uri>(Uri.EMPTY)
+    }
+
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+
+
+    val cameraLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+            capturedImageUri = uri
+        }
+    /*val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        if (it) {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            cameraLauncher.launch(uri)
+        } else {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+
+    }*/
+
+    val launcherMultiplePermissions = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissionsMap ->
+        val areGranted = permissionsMap.values.reduce { acc, next -> acc && next }
+        if (areGranted) {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(12.dp)) {
+        Button(onClick = {
+            /*val permissionCheckResult = ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA)
+
+            if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                cameraLauncher.launch(uri)
+            } else {
+                // Request a permission
+                permissionLauncher.launch(android.Manifest.permission.CAMERA)
+            }*/
+
+            if(permissions.all {
+                ContextCompat.checkSelfPermission(
+                    context,
+                    it
+                ) == PackageManager.PERMISSION_GRANTED
+            }) {
+                // Get the location
+            } else {
+                launcherMultiplePermissions.launch(permissions)
+            }
+
+        }) {
+            Text(text = "Get Current Location")
+        }
+
+        if (capturedImageUri.path?.isNotEmpty() == true) {
+            Image(
+                modifier = Modifier
+                    .padding(16.dp, 8.dp)
+                    .fillMaxWidth()
+                    .size(400.dp),
+                painter = rememberImagePainter(capturedImageUri),
+                contentDescription = null
+            )
+        }
+    }
+}
